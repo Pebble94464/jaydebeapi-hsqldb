342ba928366dec806a0129b77d94c0e21f4cea2f
Pebble94464 <jsn-github@pebble.plus.com>
2024-10-08 11:35:26 +0100

    _handle_sql_exception_jpype, translate an integrity error

--- jaydebeapi/__init__.py
+++ jaydebeapi/__init__.py
@@ -156,29 +156,21 @@ def _handle_sql_exception_jpype():
         db_err = issubclass(clazz, SQLException)
     else:
         db_err = isinstance(exc_info[1], SQLException)
-
-    integrity_error_codes = (
-        # 3500,
-        # 3501,
-        # 10,
-        # 177,
-        # 8,
-        104,	# jpype.java.sql.SQLIntegrityConstraintViolationException
-        # 157
-    )
-
     if db_err:
-        error_code = exc_info[1].getErrorCode() * -1
-        assert error_code in integrity_error_codes, 'Error code not previously seen.' #-
-        if error_code in integrity_error_codes:
+        error_code = exc_info[1].getErrorCode()
+        if isinstance(exc_info[1], (
+            jpype.java.sql.SQLIntegrityConstraintViolationException, # -104
+            )):
             exc_type = IntegrityError
+            assert old_jpype == False, "This code block is untested against older versions of jpype"
         else:
             exc_type = DatabaseError
     else:
         exc_type = InterfaceError
         
     reraise(exc_type, exc_info[1], exc_info[2])
-	# TODO: refactor. Update _handle_sql_exception_jython also.
+    # Note exceptions can also be translated at the SQLAlchemy dialect level
+    # using HyperSqlDialect.dbapi_exception_translation_map
 
 def _jdbc_connect_jpype(jclassname, url, driver_args, jars, libs):
     import jpype
